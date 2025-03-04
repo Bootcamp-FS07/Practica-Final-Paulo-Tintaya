1357952ff088899485318454f092324705889d3a
tintayadev <pau.tintaya.code@gmail.com>
2025-03-04 14:15:38 -0400

    Designed login page UI using angular material

---
+++ src/app/modules/auth/login/login.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../../shared/services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, RouterModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  username = '';
+  password = '';
+  errorMessage = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  async login() {
+    try {
+      await this.authService.login(this.username, this.password);
+      console.log('siuu')
+      this.router.navigate(['/feed']);
+    } catch (error) {
+      this.errorMessage = 'Invalid username or password';
+    }
+  }
+}
---
+++ src/app/modules/auth/signup/signup.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../../shared/services/auth.service';
+import { CommonModule, NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-signup',
+  standalone: true,
+  imports: [CommonModule, NgIf, FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, RouterModule],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.css'
+})
+export class SignupComponent {
+  username = '';
+  email = '';
+  password = '';
+  errorMessage = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  async signup() {
+    try {
+      await this.authService.signup(this.username, this.email, this.password);
+      this.router.navigate(['/auth/login']);
+    } catch (error) {
+      this.errorMessage = 'Signup failed. Try again.';
+    }
+  }
+}

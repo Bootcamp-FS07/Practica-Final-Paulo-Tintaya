8f2f35d8e66ff7061bcb5ec3cb8c45fdbcef5fd1
tintayadev <pau.tintaya.code@gmail.com>
2025-03-04 14:16:41 -0400

    Implement signup and login functinality with API integration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './modules/auth/login/login.component';
+import { SignupComponent } from './modules/auth/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'signup', component: SignupComponent },
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+    ],
+  },
+  { path: '', redirectTo: 'auth', pathMatch: 'full' },
+  { path: '**', redirectTo: 'auth' },
+];
